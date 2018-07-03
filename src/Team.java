76461878014d3eb8a71c8ee69cedd74bfabc37f5
Kunakbaev <kunakbaevav@yandex.ru>
2018-07-03 19:26:25 +0500

    Добавил поле для результата команды, методы записи индивидуальных результатов игроков и метод вывода результатов испытаний для команды.

--- src/Team.java
+++ src/Team.java
@@ -3,6 +3,11 @@ import java.util.ArrayList;
 class Team {
     private String name;
     private ArrayList<Player> players= new ArrayList<>();
+    private String resultTeam;
+
+    void setResultTeam(String resultTeam) {
+        this.resultTeam = "Команда " + name + " " + resultTeam;
+    }
 
     int run(int numberPlayer){
         return players.get(numberPlayer).getStamina();
@@ -14,6 +19,10 @@ class Team {
 
     String getNameTeam() {
         return name;
+    }
+
+    void setPlayerResults(int numberPlayer, String result){
+        players.get(numberPlayer).setResult(result);
     }
 
     Team(String name, String namePlayer1, String namePlayer2, String namePlayer3, String namePlayer4) {
@@ -28,8 +37,12 @@ class Team {
         players.add(player4);
     }
 
-    public void showResults(){
-        System.out.println("");
+    void showResults(){
+        System.out.println("Результаты испытания:");
+        for (Player player : players) {
+            System.out.println(player.getResult());
+        }
+        System.out.println(resultTeam);
     }
 
 }
